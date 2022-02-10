e98215c778c84d28b0560d1f1a391e455fdd7f37
Kshitij Srivastava <kshitij127@yahoo.co.in>
2022-02-10 09:41:00 -0800

    Added questions 2 and 3

--- Exercise_2.py
+++ Exercise_2.py
@@ -2,13 +2,40 @@
   
 # give you explanation for the approach
 def partition(arr,low,high):
-  
-  
-    #write your code here
+    pivot_idx = high
+    pivot = arr[pivot_idx]
+    #Initialize the left and right pointers
+    i = low
+    j = high - 1
+
+    while i <= j:
+
+        #advance the ith pointer to the right if its less than pivot
+        if arr[i] < pivot:
+            i += 1
+            continue
+        
+        #advance the jth pointer to the left if its more than the pivot
+        if arr[j] > pivot:
+            j -= 1
+            continue
+        #swap ith and jth pointer
+        arr[i], arr[j] = arr[j], arr[i]
+        i += 1
+        j -= 1
+    #swap the pivot and the left pointer
+    arr[pivot_idx], arr[i] = arr[i], arr[pivot_idx]
+    return i
   
 
 # Function to do Quick sort 
-def quickSort(arr,low,high): 
+def quickSort(arr,low,high):
+    if low >= high:
+        return
+    
+    pivot = partition(arr,low,high)
+    quickSort(arr, low, pivot-1) # Quicksort on data to the left to pivot
+    quickSort(arr, pivot+1, high) #Quicksort on data to the right of pivot
     
     #write your code here
   
--- Exercise_3.py
+++ Exercise_3.py
@@ -2,19 +2,42 @@
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data):
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
-    def __init__(self): 
+    def __init__(self):
+        self.head = None
         
   
-    def push(self, new_data): 
+    def push(self, new_data):
+        #pushing data at the head of the root
+        new_node = Node(new_data)
+        if self.head is None:
+            self.head = new_node
+            return
         
-  
+        new_node.next = self.head
+        self.head = new_node
+        return
+
+        
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        #initilize fast and slow pointer
+        temp1 = self.head
+        temp2 = self.head
+
+        #continue to advance both the pointers till fast pointer has node to the next of next
+        while temp2.next and temp2.next.next:
+            temp1 = temp1.next
+            temp2 = temp2.next.next
+        print(temp1.data)
+
+
 
 # Driver code 
 list1 = LinkedList() 
